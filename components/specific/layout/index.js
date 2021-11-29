b3a54a7924a85e5bd741d45827f2ebde61f3a974
m-keshavarz <m.k.dev@outlook.com>
2021-11-29 11:04:29 +0330

    background fix

--- components/specific/layout/index.js
+++ components/specific/layout/index.js
@@ -45,12 +45,25 @@ export const HeaderUser = ({ userName, avatar }) => {
 
 export const HeaderLanding = () => {
   const { lg } = useWindowSize();
+
   const [open, setOpen] = useState(false);
+  const [navBack, setNavBack] = useState(false);
+
   const menuRef = useRef(null);
   const imageRef = useRef(null);
   const navRef = useRef(null);
 
   const router = useRouter();
+
+  useEffect(() => {
+    document.addEventListener("scroll", () => {
+      if (window.scrollY > 50) {
+        setNavBack(true);
+      } else {
+        setNavBack(false);
+      }
+    });
+  });
 
   useEffect(() => {
     document.addEventListener("click", (e) => {
@@ -67,7 +80,7 @@ export const HeaderLanding = () => {
     <>
       {lg ? (
         <Div
-          className="fixed z-50 top-0 right-0 flex w-full justify-between items-center mt-3 lg:mt-6 px-[1.125rem] lg:px-[7.17%]"
+          className="absolute z-50 top-0 right-0 flex w-full justify-between items-center mt-3 lg:mt-6 px-[1.125rem] lg:px-[7.17%]"
           dir="rtl"
         >
           <Div className="flex items-center">
@@ -133,11 +146,14 @@ export const HeaderLanding = () => {
         <>
           <Div
             containerRef={navRef}
-            className="flex w-full z-50 fixed top-6 right-0 pr-7 pl-[1.125rem]"
+            className={`flex z-20 w-full fixed pt-3 top-0 right-0 pr-7 pl-[1.125rem] transition-all duration-300 ${
+              navBack &&
+              "bg-[#f0f1f4] top-0 py-2 shadow-navigation mx-[1.125rem] rounded-[0.25rem] w-[90%]"
+            }`}
           >
             <Div
               containerRef={imageRef}
-              className="w-6 h-6 relative"
+              className="w-6 h-6 relative z-40"
               onClick={() => {
                 setOpen(!open);
               }}
@@ -164,62 +180,63 @@ export const HeaderLanding = () => {
                 height={33}
               />
             </Div>
-          </Div>
-          <Div
-            containerRef={menuRef}
-            className={`fixed z-30 w-[10.25rem] top-0 -right-full transition-all duration-300 flex flex-col px-[1.75rem] pb-8 custom-backdrop-blur-nav text-sm text-Blue-700 font-SemiBold ${
-              open ? "transition-all duration-300 !right-0" : ""
-            }`}
-          >
-            <Div className="mb-8 flex flex-col">
-              <p
-                className="mt-20 mb-4"
-                onClick={() => {
-                  setOpen(!open);
-                  window.scroll({
-                    top: 3000,
-                    left: 0,
-                    behavior: "smooth",
-                  });
-                }}
-              >
-                پلن‌های اشتراک
-              </p>
-              <p
-                className="mb-4"
-                onClick={() => {
-                  setOpen(!open);
-                  window.scroll({
-                    top: 3750,
-                    left: 0,
-                    behavior: "smooth",
-                  });
-                }}
-              >
-                سرویس‌ها
-              </p>
-              <p
-                onClick={() => {
-                  setOpen(!open);
-                  window.scroll({
-                    top: 4150,
-                    left: 0,
-                    behavior: "smooth",
-                  });
-                }}
-              >
-                سوالات متداول
-              </p>
-            </Div>
-            <Div className="bg-Blue-700 w-full h-[2px]"></Div>
-            <Button
-              className="mt-4 rounded-2xl bg-[#00c485] w-full h-8 text-xs font-Medium text-white"
-              onClick={() => {
-                setOpen(!open);
-              }}
+
+            <Div
+              containerRef={menuRef}
+              className={`fixed z-30 w-[10.25rem] top-0 -right-full transition-all duration-300 flex flex-col px-[1.75rem] pb-8 custom-backdrop-blur-nav text-sm text-Blue-700 font-SemiBold ${
+                open ? "transition-all duration-300 !right-0" : ""
+              } ${navBack && "mr-[1.125rem]"}`}
             >
-              ورود/ثبت نام
-            </Button>
+              <Div className="mb-8 flex flex-col">
+                <p
+                  className="mt-20 mb-4"
+                  onClick={() => {
+                    setOpen(!open);
+                    window.scroll({
+                      top: 3000,
+                      left: 0,
+                      behavior: "smooth",
+                    });
+                  }}
+                >
+                  پلن‌های اشتراک
+                </p>
+                <p
+                  className="mb-4"
+                  onClick={() => {
+                    setOpen(!open);
+                    window.scroll({
+                      top: 3750,
+                      left: 0,
+                      behavior: "smooth",
+                    });
+                  }}
+                >
+                  سرویس‌ها
+                </p>
+                <p
+                  onClick={() => {
+                    setOpen(!open);
+                    window.scroll({
+                      top: 4150,
+                      left: 0,
+                      behavior: "smooth",
+                    });
+                  }}
+                >
+                  سوالات متداول
+                </p>
+              </Div>
+              <Div className="bg-Blue-700 w-full h-[2px]"></Div>
+              <Button
+                className="mt-4 rounded-2xl bg-[#00c485] w-full h-8 text-xs font-Medium text-white"
+                onClick={() => {
+                  setOpen(!open);
+                }}
+              >
+                ورود/ثبت نام
+              </Button>
+            </Div>
           </Div>
         </>
       )}
@@ -229,49 +246,53 @@ export const HeaderLanding = () => {
 
 export const FooterLanding = () => {
   return (
-    <Div className="flex justify-between items-center px-4 h-10 lg:px-[7.17%] w-full lg:h-[6.25rem] bg-[#1b2541]">
-      <Div className="flex">
-        <Div className="flex items-center cursor-pointer">
-          <Div className="relative w-4 h-4 ml-1 lg:w-10 lg:h-10 lg:ml-2">
-            <Image
-              alt="prodoc mail icon"
-              src="/icons/email-icon.svg"
-              layout="responsive"
-              width={40}
-              height={40}
-            />
+    <Div className="bg-[#f0f1f4]">
+      <Div
+        className={`flex justify-between items-center px-4 h-10 lg:px-[7.17%] w-[90%] mx-[1.125rem] lg:h-[6.25rem] bg-[#1b2541]`}
+      >
+        <Div className="flex">
+          <Div className="flex items-center cursor-pointer">
+            <Div className="relative w-4 h-4 ml-1 lg:w-10 lg:h-10 lg:ml-2">
+              <Image
+                alt="prodoc mail icon"
+                src="/icons/email-icon.svg"
+                layout="responsive"
+                width={40}
+                height={40}
+              />
+            </Div>
+            <CustomLink href="mailto:prodoc@gmail.com">
+              <Text className="lg:text-base text-Eight font-EnRegular text-white whitespace-nowrap">
+                prodoc@gmail.com
+              </Text>
+            </CustomLink>
           </Div>
-          <CustomLink href="mailto:prodoc@gmail.com">
-            <Text className="lg:text-base text-Eight font-EnRegular text-white whitespace-nowrap">
-              prodoc@gmail.com
-            </Text>
-          </CustomLink>
-        </Div>
-        <Div className="flex items-center lg:mr-24 mr-6 cursor-pointer">
-          <Div className="relative w-4 h-4 ml-1 lg:w-10 lg:h-10 lg:ml-2">
-            <Image
-              alt="prodoc mail icon"
-              src="/icons/phone-icon.svg"
-              layout="responsive"
-              width={40}
-              height={40}
-            />
+          <Div className="flex items-center lg:mr-24 mr-6 cursor-pointer">
+            <Div className="relative w-4 h-4 ml-1 lg:w-10 lg:h-10 lg:ml-2">
+              <Image
+                alt="prodoc mail icon"
+                src="/icons/phone-icon.svg"
+                layout="responsive"
+                width={40}
+                height={40}
+              />
+            </Div>
+            <CustomLink href="tel:+98903873829">
+              <Text className="lg:text-base text-Eight font-Regular text-white whitespace-nowrap">
+                ۰۹۰۳۸۷۳۸۲۹ | ۰۹۰۳۸۷۳۸۲۹
+              </Text>
+            </CustomLink>
           </Div>
-          <CustomLink href="tel:+98903873829">
-            <Text className="lg:text-base text-Eight font-Regular text-white whitespace-nowrap">
-              ۰۹۰۳۸۷۳۸۲۹ | ۰۹۰۳۸۷۳۸۲۹
-            </Text>
-          </CustomLink>
         </Div>
-      </Div>
-      <Div className="relative w-[2rem] h-[1.625rem] lg:w-[4.75rem] lg:h-[3.875rem]">
-        <Image
-          alt="prodoc logo"
-          src="/images/api-doc-footer-logo.svg"
-          layout="responsive"
-          width={32}
-          height={26}
-        />
+        <Div className="relative w-[2rem] h-[1.625rem] lg:w-[4.75rem] lg:h-[3.875rem]">
+          <Image
+            alt="prodoc logo"
+            src="/images/api-doc-footer-logo.svg"
+            layout="responsive"
+            width={32}
+            height={26}
+          />
+        </Div>
       </Div>
     </Div>
   );
